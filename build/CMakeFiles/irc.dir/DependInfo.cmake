4907cbe1f6311794ca8eec2e2023df85fae7ad76
sasha <pavlov.as@phystech.edu>
2022-05-20 15:10:56 +0300

    add building example

---
+++ build/CMakeFiles/irc.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/sasha/MyProjects/IRC_chat_v3/src/client.cpp" "/home/sasha/MyProjects/IRC_chat_v3/build/CMakeFiles/irc.dir/src/client.cpp.o"
+  "/home/sasha/MyProjects/IRC_chat_v3/src/console_handler.cpp" "/home/sasha/MyProjects/IRC_chat_v3/build/CMakeFiles/irc.dir/src/console_handler.cpp.o"
+  "/home/sasha/MyProjects/IRC_chat_v3/src/handler.cpp" "/home/sasha/MyProjects/IRC_chat_v3/build/CMakeFiles/irc.dir/src/handler.cpp.o"
+  "/home/sasha/MyProjects/IRC_chat_v3/src/main.cpp" "/home/sasha/MyProjects/IRC_chat_v3/build/CMakeFiles/irc.dir/src/main.cpp.o"
+  "/home/sasha/MyProjects/IRC_chat_v3/src/socket.cpp" "/home/sasha/MyProjects/IRC_chat_v3/build/CMakeFiles/irc.dir/src/socket.cpp.o"
+  "/home/sasha/MyProjects/IRC_chat_v3/src/thread.cpp" "/home/sasha/MyProjects/IRC_chat_v3/build/CMakeFiles/irc.dir/src/thread.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/irc.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/irc.dir/src/client.cpp.o"
+  "CMakeFiles/irc.dir/src/console_handler.cpp.o"
+  "CMakeFiles/irc.dir/src/handler.cpp.o"
+  "CMakeFiles/irc.dir/src/main.cpp.o"
+  "CMakeFiles/irc.dir/src/socket.cpp.o"
+  "CMakeFiles/irc.dir/src/thread.cpp.o"
+  "irc"
+  "irc.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/irc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
